854ca5c9d57c3d28f4130b0a4f05d11bbc4bf923
BryanLutz <Bryan.Lutz@united.com>
2020-01-08 15:04:01 -0500

    Update readme

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Temporarily under construction!
-        </p>
+        <p>Bryan Lutz</p>
       </header>
     </div>
   );
