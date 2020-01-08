1ba32375f570d706ad304c11bcc05ac8a10e85bd
BryanLutz <Bryan.Lutz@united.com>
2020-01-08 14:53:36 -0500

    Minor cleanup

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Temporarily under construction!/i);
   expect(linkElement).toBeInTheDocument();
 });
