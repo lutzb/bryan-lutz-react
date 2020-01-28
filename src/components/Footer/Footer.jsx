ffc0cca5e569cc1b2dce4342a42d3f54af04f899
BryanLutz <Bryan.Lutz@united.com>
2020-01-28 10:16:26 -0500

    Added Github and Twitter logos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Footer.scss'
 import LinkedInLogo from '../../img/LinkedIn-White-66px-TM.png'
+import GitHubLogo from '../../img/GitHub-Mark-Light-64px.png'
+import TwitterLogo from '../../img/Twitter_Social_Icon_Circle_White.svg'
 
 const Footer = () => (
   <div className='footer'>
     <p>Bryan Lutz • Columbus, Ohio • 43221</p>
-    <a href='https://www.linkedin.com/in/bryan-lutz-080a6a57/' target='_blank'>
-      <img src={LinkedInLogo} className='social-logo' alt=''/>
-    </a>
+    <div>
+      <a href='https://www.linkedin.com/in/bryan-lutz-080a6a57/' target='_blank'>
+        <img src={LinkedInLogo} className='social-logo' alt=''/>
+      </a>
+      <a href='https://github.com/lutzb' target='_blank'>
+        <img src={GitHubLogo} className='social-logo' alt=''/>
+      </a>
+      <a href='https://twitter.com/blutzer' target='_blank'>
+        <img src={TwitterLogo} className='social-logo' alt=''/>
+      </a>
+    </div>
   </div>
 )
 
