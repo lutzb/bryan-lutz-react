d6fca643ccda43468314feb9df0a80877c87425f
BryanLutz <Bryan.Lutz@united.com>
2020-01-31 14:14:33 -0500

    Add icons to home tiles

--- src/components/HomeTile/HomeTile.jsx
+++ src/components/HomeTile/HomeTile.jsx
@@ -8,7 +8,8 @@ class HomeTile extends React.Component {
         <div className="card">
           <img src={this.props.image} className='card-image'></img>
           <div className="container">
-            <h3>{this.props.header}</h3>
+            <h3>{this.props.header1}</h3>
+            <h3>{this.props.header2}</h3>
             <p>{this.props.text}</p>
           </div>
         </div>
--- src/components/HomeTiles/HomeTiles.jsx
+++ src/components/HomeTiles/HomeTiles.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import './HomeTiles.scss'
 import HomeTile from '../HomeTile/HomeTile'
-import TwitterLogo from '../../img/Twitter_Social_Icon_Circle_White.svg'
+import CodeLogo from '../../img/code.png'
+import DataLogo from '../../img/data.jpg'
+import EsportsLogo from '../../img/esports.jpg'
 
 const HomeTiles = () => (
   <div className='home-tiles'>
-    <HomeTile image={TwitterLogo} header='Show me the code.' text='Unyielding advocate of ATDD, clean code, and extreme programming.' />
-    <HomeTile image={TwitterLogo} header='Data is my passion.' text='Big data, ETL, data transformation, and data analytics.' />
-    <HomeTile image={TwitterLogo} header='Embrace Esports.' text='Over $1 Billion dollar industry for playing video games competitively. Yeah, really.' />
+    <HomeTile image={CodeLogo} header1='Talk is cheap' header2='Show me the code' text='Unyielding advocate of ATDD, clean code, and extreme programming.' />
+    <HomeTile image={DataLogo} header1='Everything is data' header2='He who has the most, wins' text='Data is my passion. Big data, ETL, data transformation, and data analytics.' />
+    <HomeTile image={EsportsLogo} header1='Forget traditional sports' header2='Embrace Esports' text='Kids are gaining fortune and fame for playing video games competitively in an over $1 billion industry' />
   </div>
 )
-
-/*
-Unyielding advocate of TDD, clean code, and YAGNI extreme programming
-Back-end data transformation, ETL, data manipulation, Batch script orchestration.
-Identifying and encouraging the use of Software Development industry patterns.
-
-*/
 
 export default HomeTiles
